d224bd37f92180e9c28ea33e0131c5722f926777
Olivier Cailloux <olivier.cailloux@dauphine.psl.eu>
2026-05-20 23:50:07 +0200

    record to class

--- src/main/java/io/github/oliviercailloux/grading/CompositeAssessmentTree.java
+++ src/main/java/io/github/oliviercailloux/grading/CompositeAssessmentTree.java
@@ -1,14 +1,52 @@
 package io.github.oliviercailloux.grading;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableMap;
+import java.io.Serializable;
 import java.util.Map;
 import java.util.Objects;
 
-/** Composite node keyed by criterion label. */
-public record CompositeAssessmentTree(ImmutableMap<String, AssessmentTree> children) implements AssessmentTree {
-  public CompositeAssessmentTree {
+@SuppressWarnings("serial")
+public final class CompositeAssessmentTree implements AssessmentTree, Serializable {
+  public static CompositeAssessmentTree given(Map<Criterion, ? extends AssessmentTree> children) {
+    return new CompositeAssessmentTree(children);
+  }
+
+  private final ImmutableMap<Criterion, AssessmentTree> children;
+
+  CompositeAssessmentTree(Map<Criterion, ? extends AssessmentTree> children) {
+    checkNotNull(children);
     checkArgument(!children.isEmpty(), "Children must be non-empty.");
+    for (Map.Entry<Criterion, ? extends AssessmentTree> entry : children.entrySet()) {
+      checkNotNull(entry.getKey());
+      checkNotNull(entry.getValue());
+    }
+    this.children = ImmutableMap.copyOf(children);
+  }
+
+  public ImmutableMap<Criterion, AssessmentTree> getChildren() {
+    return children;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (!(o instanceof CompositeAssessmentTree that))
+      return false;
+    return children.equals(that.children);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(children);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this).add("children", children).toString();
   }
 }
