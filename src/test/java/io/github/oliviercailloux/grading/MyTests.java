b6db96ecbe7cedf1e5c16752292f16b0253b305d
Olivier Cailloux <olivier.cailloux@dauphine.psl.eu>
2026-05-20 21:33:34 +0200

    testing instances conformity

--- src/test/java/io/github/oliviercailloux/grading/MyTests.java
+++ src/test/java/io/github/oliviercailloux/grading/MyTests.java
@@ -1,14 +1,15 @@
 package io.github.oliviercailloux.grading;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.google.common.collect.ImmutableList;
+import com.networknt.schema.Error;
 import com.networknt.schema.InputFormat;
 import com.networknt.schema.Schema;
 import com.networknt.schema.SchemaLocation;
 import com.networknt.schema.SchemaRegistry;
-import com.networknt.schema.SchemaRegistryConfig;
-import com.networknt.schema.SpecificationVersion;
 import com.networknt.schema.dialect.Dialects;
-import com.networknt.schema.regex.JoniRegularExpressionFactory;
-import com.networknt.schema.Error;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -19,74 +20,79 @@ public class MyTests {
   private static final Logger LOGGER = LoggerFactory.getLogger(MyTests.class);
 
   @Test
-  void testSchema() throws Exception {
+  void testWrongSchema() throws Exception {
     SchemaRegistry schemaRegistry = SchemaRegistry.withDialect(Dialects.getDraft202012());
-    /*
-     * Due to the mapping the meta-schema for the dialect will be retrieved from the classpath at
-     * classpath:draft/2020-12/schema.
-     */
-    Schema schema = schemaRegistry.getSchema(SchemaLocation.of(Dialects.getDraft202012().getId()));
+    Schema draftSchema = schemaRegistry.getSchema(SchemaLocation.of(Dialects.getDraft202012().getId()));
     String input = "{\r\n" + "  \"type\": \"object\",\r\n" + "  \"properties\": {\r\n"
         + "    \"key\": {\r\n" + "      \"title\" : \"My key\",\r\n"
         + "      \"type\": \"invalidtype\"\r\n" + "    }\r\n" + "  }\r\n" + "}";
-    List<Error> errors = schema.validate(input, InputFormat.JSON, executionContext -> {
-      /*
-       * By default since Draft 2019-09 the format keyword only generates annotations and not
-       * assertions.
-       */
-      executionContext
-          .executionConfig(executionConfig -> executionConfig.formatAssertionsEnabled(true));
-    });
+    List<Error> errors = draftSchema.validate(input, InputFormat.JSON);
+    assertEquals(2, errors.size());
+    Error e0 = errors.get(0);
+    assertTrue(e0.getMessage().contains("valeur dans l'énumération"), e0.getMessage());
+    Error e1 = errors.get(1);
+    assertTrue(e1.getMessage().contains("array attendu"), e1.getMessage());
   }
 
   @Test
-  void testSomething() throws Exception {
-    LOGGER.info("Started tests.");
-    /*
-     * The SchemaRegistryConfig can be optionally used to configure certain aspects of how the
-     * validation is performed.
-     *
-     * By default the JDK regular expression implementation which is not ECMA 262 compliant is used.
-     * The GraalJSRegularExpressionFactory.getInstance() offers the best compliance followed by
-     * JoniRegularExpressionFactory.getInstance() but both require additional optional dependencies.
-     */
-    SchemaRegistryConfig schemaRegistryConfig = SchemaRegistryConfig.builder()
-        .regularExpressionFactory(JoniRegularExpressionFactory.getInstance()).build();
+  void testGoodSchema() throws Exception {
+    SchemaRegistry schemaRegistry = SchemaRegistry.withDialect(Dialects.getDraft202012());
+    Schema draftSchema = schemaRegistry.getSchema(SchemaLocation.of(Dialects.getDraft202012().getId()));
+    String markTreeSchema = Resourcer.charSource("schemas/mark-tree.schema.json").read();
+    assertEquals(ImmutableList.of(), draftSchema.validate(markTreeSchema, InputFormat.JSON));
+  }
 
-    /*
-     * This creates a schema registry that supports all the standard dialects for cross-dialect
-     * validation and will use Draft 2020-12 as the default if $schema is not specified in the
-     * schema data. If $schema is specified in the schema data then that schema dialect will be used
-     * instead and this version is ignored.
-     */
-    SchemaRegistry schemaRegistry =
-        SchemaRegistry.withDefaultDialect(SpecificationVersion.DRAFT_2020_12,
-            builder -> builder.schemaRegistryConfig(schemaRegistryConfig)
-                /*
-                 * This creates a mapping from $id which starts with https://www.example.org/schema
-                 * to the retrieval IRI classpath:schema.
-                 */
+  @Test
+  void testMark() throws Exception {
+    SchemaRegistry schemaRegistry = SchemaRegistry.withDialect(Dialects.getDraft202012(), builder -> builder
                 .schemaIdResolvers(schemaIdResolvers -> schemaIdResolvers
-                    .mapPrefix("https://www.example.com/schema", "classpath:schema")));
+                    .mapPrefix("https://raw.githubusercontent.com/oliviercailloux/grading-schema/main/mark-tree.schema.json", "classpath:/io/github/oliviercailloux/grading/schemas/mark-tree.schema.json")));
+    Schema markTreeSchema = schemaRegistry
+        .getSchema(SchemaLocation.of("https://raw.githubusercontent.com/oliviercailloux/grading-schema/main/mark-tree.schema.json"));
 
-    /*
-     * Due to the mapping the schema will be retrieved from the classpath at
-     * classpath:schema/example-main.json. If the schema data does not specify an $id the absolute
-     * IRI of the schema location will be used as the $id. If the schema data does not specify a
-     * dialect using $schema the default dialect specified when creating the schema registry.
-     */
-    Schema schema = schemaRegistry
-        .getSchema(SchemaLocation.of("https://www.example.com/schema/example-main.json"));
-    String input = "{\r\n" + "  \"main\": {\r\n" + "    \"common\": {\r\n"
-        + "      \"field\": \"invalidfield\"\r\n" + "    }\r\n" + "  }\r\n" + "}";
+    List<Error> errors = markTreeSchema.validate(Resourcer.charSource("Mark.json").read(), InputFormat.JSON);
+    assertEquals(0, errors.size());
+  }
 
-    List<Error> errors = schema.validate(input, InputFormat.JSON, executionContext -> {
-      /*
-       * By default since Draft 2019-09 the format keyword only generates annotations and not
-       * assertions.
-       */
-      executionContext
-          .executionConfig(executionConfig -> executionConfig.formatAssertionsEnabled(true));
-    });
+  @Test
+  void testWrongMark() throws Exception {
+    SchemaRegistry schemaRegistry = SchemaRegistry.withDialect(Dialects.getDraft202012(), builder -> builder
+                .schemaIdResolvers(schemaIdResolvers -> schemaIdResolvers
+                    .mapPrefix("https://raw.githubusercontent.com/oliviercailloux/grading-schema/main/mark-tree.schema.json", "classpath:/io/github/oliviercailloux/grading/schemas/mark-tree.schema.json")));
+    Schema markTreeSchema = schemaRegistry
+        .getSchema(SchemaLocation.of("https://raw.githubusercontent.com/oliviercailloux/grading-schema/main/mark-tree.schema.json"));
+
+    List<Error> errors = markTreeSchema.validate(Resourcer.charSource("Wrong mark.json").read(), InputFormat.JSON);
+    assertEquals(1, errors.size());
+    assertEquals("unevaluatedProperties", errors.get(0).getMessageKey());
+  }
+
+  @Test
+  void testComposite() throws Exception {
+    SchemaRegistry schemaRegistry = SchemaRegistry.withDialect(Dialects.getDraft202012(), builder -> builder
+                .schemaIdResolvers(schemaIdResolvers -> schemaIdResolvers
+                    .mapPrefix("https://raw.githubusercontent.com/oliviercailloux/grading-schema/main/mark-tree.schema.json", "classpath:/io/github/oliviercailloux/grading/schemas/mark-tree.schema.json")));
+    Schema markTreeSchema = schemaRegistry
+        .getSchema(SchemaLocation.of("https://raw.githubusercontent.com/oliviercailloux/grading-schema/main/mark-tree.schema.json"));
+
+    List<Error> errors = markTreeSchema.validate(Resourcer.charSource("Children.json").read(), InputFormat.JSON);
+    assertEquals(0, errors.size());
+  }
+
+  @Test
+  void testWrongComposite() throws Exception {
+    SchemaRegistry schemaRegistry = SchemaRegistry.withDialect(Dialects.getDraft202012(), builder -> builder
+                .schemaIdResolvers(schemaIdResolvers -> schemaIdResolvers
+                    .mapPrefix("https://raw.githubusercontent.com/oliviercailloux/grading-schema/main/mark-tree.schema.json", "classpath:/io/github/oliviercailloux/grading/schemas/mark-tree.schema.json")));
+    Schema markTreeSchema = schemaRegistry
+        .getSchema(SchemaLocation.of("https://raw.githubusercontent.com/oliviercailloux/grading-schema/main/mark-tree.schema.json"));
+
+    List<Error> errors = markTreeSchema.validate(Resourcer.charSource("Wrong children.json").read(), InputFormat.JSON);
+    assertEquals(12, errors.size());
+    assertEquals("oneOf", errors.get(0).getMessageKey());
+    assertEquals("required", errors.get(1).getMessageKey());
+    assertEquals("oneOf", errors.get(2).getMessageKey());
+    assertEquals("required", errors.get(3).getMessageKey());
+    assertEquals("unevaluatedProperties", errors.get(4).getMessageKey());
   }
 }
