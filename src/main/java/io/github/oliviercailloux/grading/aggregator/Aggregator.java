140ad3579046143c10cfab78730d99f10b4eec40
Olivier Cailloux <olivier.cailloux@dauphine.psl.eu>
2026-05-23 00:58:37 +0200

    started impl

---
+++ src/main/java/io/github/oliviercailloux/grading/aggregator/Aggregator.java
@@ -0,0 +1,36 @@
+package io.github.oliviercailloux.grading.aggregator;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.collect.ImmutableMap;
+import io.github.oliviercailloux.grading.Criterion;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public sealed abstract class Aggregator
+    permits Parametric, Weighter, Owa {
+
+  private final ImmutableMap<Criterion, Aggregator> subs;
+  private final Optional<Aggregator> defaultSub;
+
+  protected Aggregator(Map<Criterion, Aggregator> subs, Optional<Aggregator> defaultSub) {
+    this.subs = ImmutableMap.copyOf(subs);
+    this.defaultSub = checkNotNull(defaultSub);
+  }
+
+  public ImmutableMap<Criterion, Aggregator> subs(Set<Criterion> criteria) {
+    checkNotNull(criteria);
+    ImmutableMap.Builder<Criterion, Aggregator> subAggregators = ImmutableMap.builder();
+    for (Criterion criterion : criteria) {
+      if (subs.containsKey(criterion)) {
+        subAggregators.put(criterion, subs.get(criterion));
+      } else {
+        defaultSub.ifPresent(agg -> subAggregators.put(criterion, agg));
+      }
+    }
+    return subAggregators.build();
+  }
+
+  public abstract double aggregate(Map<Criterion, Double> marks);
+}
---
+++ src/main/java/io/github/oliviercailloux/grading/aggregator/Parametric.java
@@ -0,0 +1,14 @@
+package io.github.oliviercailloux.grading.aggregator;
+
+import io.github.oliviercailloux.grading.Criterion;
+import java.util.Map;
+
+public final class Parametric extends Aggregator {
+
+  @Override
+  public double agg(Map<Criterion, Double> marks) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'agg'");
+  }
+
+}
