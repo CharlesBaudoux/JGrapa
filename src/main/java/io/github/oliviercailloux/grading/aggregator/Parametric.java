7c07a275074ee61fe4dc3e40b8e6a5fb817f7426
Olivier Cailloux <olivier.cailloux@dauphine.psl.eu>
2026-05-28 23:45:29 +0200

    using weighted sum

--- src/main/java/io/github/oliviercailloux/grading/aggregator/Parametric.java
+++ src/main/java/io/github/oliviercailloux/grading/aggregator/Parametric.java
@@ -2,11 +2,13 @@ package io.github.oliviercailloux.grading.aggregator;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Verify.verify;
 
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
+import com.google.common.collect.Sets;
 import io.github.oliviercailloux.grading.Criterion;
 import io.github.oliviercailloux.grading.assessment.Mark;
 import java.util.Map;
@@ -25,14 +27,14 @@ public final class Parametric extends Aggregator {
   public static Parametric given(Criterion multiplied, Criterion weighting) {
     return new Parametric(multiplied, weighting, Map.of(), Optional.empty());
   }
-  
+
   public static Parametric given(Criterion multiplied, Criterion weighting,
       Map<Criterion, Aggregator> subs, Optional<Aggregator> defaultSub) {
     return new Parametric(multiplied, weighting, subs, defaultSub);
   }
 
-  private Parametric(Criterion multiplied, Criterion weighting,
-      Map<Criterion, Aggregator> subs, Optional<Aggregator> defaultSub) {
+  private Parametric(Criterion multiplied, Criterion weighting, Map<Criterion, Aggregator> subs,
+      Optional<Aggregator> defaultSub) {
     super(subs, defaultSub);
     this.multiplied = checkNotNull(multiplied);
     this.weighting = checkNotNull(weighting);
@@ -42,18 +44,32 @@ public final class Parametric extends Aggregator {
 
   @Override
   public double aggregate(OneLevelMarksTree marks) {
-    ImmutableSet<Criterion> otherCriteria = marks.map().keySet().stream()
-        .filter(criterion -> !criterion.equals(multiplied) && !criterion.equals(weighting))
-        .collect(ImmutableSet.toImmutableSet());
-    double weightingMark = marks.map().getOrDefault(weighting, Mark.max()).value();
-    ImmutableMap.Builder<Criterion, Double> weightsBuilder = ImmutableMap.builder();
-    weightsBuilder.put(multiplied, weightingMark);
-    otherCriteria.forEach(criterion -> weightsBuilder.put(criterion, (1d - weightingMark) / otherCriteria.size()));
-    ImmutableMap<Criterion, Double> effectiveWeights = weightsBuilder.build();
-    double result = effectiveWeights.keySet().stream()
-        .mapToDouble(criterion -> marks.map().getOrDefault(criterion, Mark.max()).value() * effectiveWeights.get(criterion))
-        .sum();
-    return result;
+    ImmutableMap<Criterion, Double> effectiveWeights;
+    {
+      double weightingMark = marks.map().getOrDefault(weighting, Mark.max()).value();
+      ImmutableMap.Builder<Criterion, Double> weightsBuilder = ImmutableMap.builder();
+      ImmutableSet<Criterion> aPrioriCriteria = ImmutableSet.of(weighting, multiplied);
+      ImmutableSet<Criterion> otherCriteria =
+          Sets.difference(marks.map().keySet(), aPrioriCriteria).immutableCopy();
+      weightsBuilder.put(multiplied, weightingMark);
+      otherCriteria.forEach(
+          criterion -> weightsBuilder.put(criterion, (1d - weightingMark) / otherCriteria.size()));
+      effectiveWeights = weightsBuilder.build();
+    }
+    final ImmutableMap<Criterion, Mark> effectiveMarks;
+    {
+      ImmutableMap.Builder<Criterion, Mark> builder = ImmutableMap.<Criterion, Mark>builder();
+      marks.map().keySet().stream().filter(criterion -> !criterion.equals(weighting))
+          .forEach(criterion -> builder.put(criterion, marks.map().get(criterion)));
+      if (!marks.map().containsKey(multiplied)) {
+        builder.put(multiplied, Mark.max());
+      }
+      effectiveMarks = builder.build();
+    }
+    verify(effectiveMarks.keySet().equals(effectiveWeights.keySet()),
+        "The effective marks and weights must be defined on the same criteria.");
+    return Aggregator.WeightedMarks.given(OneLevelMarksTree.given(effectiveMarks), effectiveWeights)
+        .weightedSum();
   }
 
   public Criterion multiplied() {
@@ -83,15 +99,15 @@ public final class Parametric extends Aggregator {
     return multiplied.equals(t2.multiplied) && weighting.equals(t2.weighting)
         && subs().equals(t2.subs()) && defaultSub().equals(t2.defaultSub());
   }
-  
+
   @Override
   public int hashCode() {
     return Objects.hash(multiplied, weighting, subs(), defaultSub());
   }
-  
+
   @Override
   public String toString() {
-    return MoreObjects.toStringHelper(this).add("multiplied", multiplied).add("weighting", weighting)
-        .add("subs", subs()).add("defaultSub", defaultSub()).toString();
+    return MoreObjects.toStringHelper(this).add("multiplied", multiplied)
+        .add("weighting", weighting).add("subs", subs()).add("defaultSub", defaultSub()).toString();
   }
 }
