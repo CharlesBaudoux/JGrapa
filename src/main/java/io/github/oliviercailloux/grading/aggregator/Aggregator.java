48d30654003813f48222f0eb970af636a250dc86
Olivier Cailloux <olivier.cailloux@dauphine.psl.eu>
2026-05-28 22:51:02 +0200

    WeightedMarks class

--- src/main/java/io/github/oliviercailloux/grading/aggregator/Aggregator.java
+++ src/main/java/io/github/oliviercailloux/grading/aggregator/Aggregator.java
@@ -4,13 +4,74 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.math.DoubleMath;
 import io.github.oliviercailloux.grading.Criterion;
 import io.github.oliviercailloux.grading.assessment.Mark;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.Set;
 
 public sealed abstract class Aggregator permits Parametric, Weighter, Owa {
+  public static class WeightedMarks {
+    private static final double TOLERANCE = 1e-9;
+    private final ImmutableMap<Criterion, Mark> marks;
+    private final ImmutableMap<Criterion, Double> weights;
+
+    private WeightedMarks(Map<Criterion, Mark> marks, Map<Criterion, Double> weights) {
+      checkArgument(weights.values().stream().allMatch(w -> Double.isFinite(w)),
+      "All weights must be finite.");
+      checkArgument(weights.values().stream().allMatch(w -> 0d <= w),
+      "All weights must be non-negative.");
+      double totalWeight = weights.values().stream().mapToDouble(Double::doubleValue).sum();
+      checkArgument(DoubleMath.fuzzyEquals(totalWeight, 1d, TOLERANCE), "Total weight must be one.");
+      checkArgument(marks.keySet().equals(weights.keySet()), "Marks and weights must have the same criteria.");
+      this.marks = ImmutableMap.copyOf(marks);
+      this.weights = ImmutableMap.copyOf(weights);
+    }
+
+    /**
+     * @return not empty
+     */
+    public ImmutableMap<Criterion, Mark> marks() {
+      return marks;
+    }
+    
+    /**
+     * @return sum to one, non-negative, no NaN
+     */
+    public ImmutableMap<Criterion, Double> weights() {
+      return weights;
+    }
+
+    public ImmutableSet<Criterion> criteria() {
+      return marks.keySet();
+    }
+    
+    public Mark mark(Criterion criterion) {
+      if(!marks.containsKey(criterion)) {
+        throw new NoSuchElementException("Criterion not found: " + criterion);
+      }
+      return marks.get(criterion);
+    }
+
+    public double weight(Criterion criterion) {
+      if(!weights.containsKey(criterion)) {
+        throw new NoSuchElementException("Criterion not found: " + criterion);
+      }
+      return weights.get(criterion);
+    }
+
+    public double weightedAverage() {
+      double result = 0d;
+      for (Criterion criterion : criteria()) {
+        result += mark(criterion).value() * weight(criterion);
+      }
+      return result;
+    }
+  }
+
   public static record OneLevelMarksTree (/**
                                            * non-empty
                                            */
